2e6b7de493871a606b4ac80b42d5396d840b3139
Rahmir Ford <Rahmir.ford@gmail.com>
2017-05-29 13:52:10 -0400

    Added images and buttons to clieent interface

--- DealerSpec.js
+++ DealerSpec.js
@@ -21,5 +21,12 @@ it("The last card in the deck should not be an Ace of Spades", function(){
      var dealer = new Dealer();
      expect(dealer.dealRandomCard().toString()).not.toEqual("Ace of Spades");
     });
+
+it("The deck should decrease in size as card is delt", function(){
+    var dealer = new Dealer();
+    dealer.dealRandomCard();
+    dealer.dealRandomCard();
+    expect(dealer.deck.deckOfCards.length).toBe(310);
+    });
 });
     
