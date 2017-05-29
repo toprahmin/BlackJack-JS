6673e91bdbaf21e2b8fc902585a36e2a6796d7fd
Rahmir Ford <Rahmir.ford@gmail.com>
2017-05-29 13:17:22 -0400

    Dealer deal method complete

--- Dealer.js
+++ Dealer.js
@@ -9,14 +9,20 @@ class Dealer{
         
         }
     }
-
-    shuffleDeck(){
+// TEST
+    dealRandomCard(){
+        var randomIndex = Math.floor(Math.random() * this.deck.deckOfCards.length);
+        var randomCard = this.deck.deckOfCards.splice(randomIndex,1);
+        return randomCard;
+    }
+       /* 
         for (var i = this.deck.deckOfCards - 1; i > 0; i--){
             var k = Math.floor(Math.random()*(i+1));
             var shuffledCardArray = this.deck.deckOfCards[i];
             this.deck.deckOfCards[i] = this.deck.deckOfCards[k];
             this.deck.deckOfCards[k] = shuffledCardArray;
         }
-        return shuffledCardArray;
-    }
+        return this.deck.deckOfCards;
+    
+    */
 }
--- DealerSpec.js
+++ DealerSpec.js
@@ -1,21 +1,25 @@
+
 describe("Dealer", function(){
-    
+/*    
         var dealer;
         beforeEach(function(){
         dealer = new Dealer();
 
     });
-
+*/
      it("There should be 312 cards in the deck",function(){
+        var dealer = new Dealer();
         expect(dealer.deck.deckOfCards.length).toBe(312);
     });
 
     it("The last card in the deck should be Ace of Spade", function(){
+        var dealer = new Dealer();
         expect(dealer.deck.deckOfCards[311].toString()).toEqual("Ace of Spades");
     });
-
-    it("The last card in the deck should not be an Ace of Spades", function(){
-        expect(dealer.shuffleDeck.toString()).toEqual("Ace of Spades");
+//TEST
+it("The last card in the deck should not be an Ace of Spades", function(){
+     var dealer = new Dealer();
+     expect(dealer.dealRandomCard().toString()).not.toEqual("Ace of Spades");
     });
 });
     
